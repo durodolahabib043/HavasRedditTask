31459b9743a549c2e283d4eac27d5eb38ea0ad6a
habib <durodolahabib043@gmail.com>
2021-03-13 14:44:03 -0500

    added colors

--- HavasRedditTask/Extensions/Extensions.swift
+++ HavasRedditTask/Extensions/Extensions.swift
@@ -7,6 +7,12 @@
 
 import UIKit
 
+extension UIColor {
+    static func setUIColor(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red / 255, green: green / 255, blue: blue / 255, alpha: 1.0)
+    }
+}
+
 extension UILabel {
     static func newLabel() -> UILabel {
         let label = UILabel()
